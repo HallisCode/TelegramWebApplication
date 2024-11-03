d2379d7e72e80b7e8e677f707de8b90a91261b6b
Andrey <halliscode.js@gmail.com>
2024-11-03 19:15:29 +0300

    Добавил логгировавние по умолчанию

--- TelegramWebApp/TelegramWebApplicationBuilder.cs
+++ TelegramWebApp/TelegramWebApplicationBuilder.cs
@@ -19,6 +19,8 @@ namespace TelegramWebApp
         public void SetServer(IServer server)
         {
             Server = server;
+
+            Logging.AddConsole();
         }
 
         public TelegramWebApplication Build()
